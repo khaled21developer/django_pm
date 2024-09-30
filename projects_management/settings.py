a46e6e3b94f4a0dce1e85f5b9c017054f3b9a1ee
khaled21developer <kh.hammad2022@gmail.com>
2024-10-01 00:06:02 +0200

    Initial commit

--- projects_management/settings.py
+++ projects_management/settings.py
@@ -76,17 +76,17 @@ WSGI_APPLICATION = 'projects_management.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
 
-# 'default': {
-      #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-      #     'NAME': 'django_pm',
-      #     'USER': 'postgres',
-      #     'PASSWORD': 'khaled2001',
-      #     'HOST' : 'localhost',
-      #     'PORT': ''
+'default': {
+          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+          'NAME': 'django_pm',
+          'USER': 'postgres',
+          'PASSWORD': 'khaled2001',
+          'HOST': 'localhost',
+          'PORT': ''
     }
 }
 
