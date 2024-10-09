d93eb432364fd74491b08cd06656d8abc7a2f393
khaled21developer <kh.hammad2022@gmail.com>
2024-10-09 16:29:14 +0200

    صفحة إدارة المشروع

---
+++ projects/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from . import models
+
+class ProjectCreateForm(forms.ModelForm):
+    class Meta:
+        model = models.Project
+        fields = ['category', 'title', 'description']
+        widgets = {
+            'category': forms.Select(),
+            'title': forms.TextInput(),
+            'description': forms.Textarea(),
+        }
+
+
+class ProjectUpdateForm(forms.ModelForm):
+    class Meta:
+        model = models.Project
+        fields = ['category', 'title', 'status']
+        widgets = {
+            'category': forms.Select(),
+            'title': forms.TextInput(),
+            'status': forms.Select(),
+        }
